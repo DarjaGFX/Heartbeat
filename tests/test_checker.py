8395160db8c631a248990bfe266fa094385e2234
Ali Jafari <ali.jafari20@gmail.com>
2025-02-02 07:13:23 +0330

    remove redis tests

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,96 +1,96 @@
 import pytest
-from redis import Redis
+# from redis import Redis
 
-from app.src.checker import journalctl, online_service, systemd_service_status
+# from app.src.checker import journalctl, online_service, systemd_service_status
 
-pytest_plugins = ('pytest_asyncio',)
+# pytest_plugins = ('pytest_asyncio',)
 
-r = Redis()
+# r = Redis()
 
-r.sadd('services', 'redis', 'google')
+# r.sadd('services', 'redis', 'google')
 
-r.hset(
-    name="tor",
-    mapping={
-        "type": "systemd",
-        "service_name": "redis",
-        "period_sec": "5"
-    }
-)
-r.hset(
-    name="google",
-    mapping= {
-        "type": "online",
-        "method": "get",
-        "url": "https://google.com",
-        "period_sec": "10"
-    }
-)
+# r.hset(
+#     name="tor",
+#     mapping={
+#         "type": "systemd",
+#         "service_name": "redis",
+#         "period_sec": "5"
+#     }
+# )
+# r.hset(
+#     name="google",
+#     mapping= {
+#         "type": "online",
+#         "method": "get",
+#         "url": "https://google.com",
+#         "period_sec": "10"
+#     }
+# )
 
 
-@pytest.mark.asyncio
-async def test_online_service_status_code():
-    """
-    test Online service check by status code
-    """
-    res = await online_service(method='get', url='https://google.com')
-    if isinstance(res, bool):
-        assert res is True
-    else:
-        assert res[0] is True
+# @pytest.mark.asyncio
+# async def test_online_service_status_code():
+#     """
+#     test Online service check by status code
+#     """
+#     res = await online_service(method='get', url='https://google.com')
+#     if isinstance(res, bool):
+#         assert res is True
+#     else:
+#         assert res[0] is True
     # assert Oservice(method='get', url='https://mail.google.com/mail/u/0/#inbox') == False
 
 
-@pytest.mark.asyncio
-async def test_online_service_operator():
-    """
-    test Online service by different operators
-    """
-    res = await online_service(
-        method='get',
-        url='https://google.com',
-        desired_response='script',
-        target='content',
-        operator='in'
-        )
-    if isinstance(res, bool):
-        assert res is True
-    else:
-        assert res[0] is True
-    res = await online_service(
-        method='get',
-        url='https://google.com',
-        desired_response='script',
-        target='content',
-        operator='not in'
-        )
-    assert res is False
+# @pytest.mark.asyncio
+# async def test_online_service_operator():
+#     """
+#     test Online service by different operators
+#     """
+#     res = await online_service(
+#         method='get',
+#         url='https://google.com',
+#         desired_response='script',
+#         target='content',
+#         operator='in'
+#         )
+#     if isinstance(res, bool):
+#         assert res is True
+#     else:
+#         assert res[0] is True
+#     res = await online_service(
+#         method='get',
+#         url='https://google.com',
+#         desired_response='script',
+#         target='content',
+#         operator='not in'
+#         )
+#     assert res is False
 
 
-@pytest.mark.asyncio
-async def test_systemd():
-    """
-    test systemd service status
-    """
-    assert await systemd_service_status(service_name='redis') is True
-    assert await systemd_service_status(service_name='Noservice') is False
+# @pytest.mark.asyncio
+# async def test_systemd():
+#     """
+#     test systemd service status
+#     """
+#     assert await systemd_service_status(service_name='redis') is True
+#     assert await systemd_service_status(service_name='Noservice') is False
 
 
-@pytest.mark.asyncio
-async def test_journalctl():
-    """
-    test journalctl status report
-    """
-    assert await journalctl(
-        service_name='systemd-journald.service',
-        desired_response=':',
-        operator='in'
-        ) is True
-    assert await journalctl(
-        service_name='systemd-journald.service',
-        desired_response='checker',
-        operator='in'
-    ) is False
+# @pytest.mark.asyncio
+# async def test_journalctl():
+#     """
+#     test journalctl status report
+#     """
+#     assert await journalctl(
+#         service_name='systemd-journald.service',
+#         desired_response=':',
+#         operator='in'
+#         ) is True
+#     assert await journalctl(
+#         service_name='systemd-journald.service',
+#         desired_response='checker',
+#         operator='in'
+#     ) is False
 
 # s = r.smembers('services')
 # services =  [i.decode(encoding='utf-8') for i in s]
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,73 +1,73 @@
-import pytest
-from redis import Redis
+# import pytest
+# from redis import Redis
 
-from app.db.helper import (add_service, get_service_config, get_services,
-                                 remove_service)
+# from app.db.helper import (add_service, get_service_config, get_services,
+#                                  remove_service)
 
-pytest_plugins = ('pytest_asyncio',)
+# pytest_plugins = ('pytest_asyncio',)
 
-r = Redis()
+# r = Redis()
 
-r.sadd('services', 'redis', 'google')
+# r.sadd('services', 'redis', 'google')
 
-r.hset(
-    name="redis",
-    mapping={
-        "type": "systemd",
-        "service_name": "redis",
-        "period_sec": "5"
-    }
-)
-r.hset(
-    name="google",
-    mapping= {
-        "type": "online",
-        "method": "get",
-        "url": "https://google.com",
-        "period_sec": "10"
-    }
-)
+# r.hset(
+#     name="redis",
+#     mapping={
+#         "type": "systemd",
+#         "service_name": "redis",
+#         "period_sec": "5"
+#     }
+# )
+# r.hset(
+#     name="google",
+#     mapping= {
+#         "type": "online",
+#         "method": "get",
+#         "url": "https://google.com",
+#         "period_sec": "10"
+#     }
+# )
 
-@pytest.mark.asyncio
-async def test_load_basic_servies():
-    """
-    test load service
-    """
-    assert sorted(await get_services()) == sorted(['redis', 'google'])
-
-
-@pytest.mark.asyncio
-async def test_add_servies():
-    """
-    test add new service
-    """
-    await add_service({
-        "service_name": "yahoo",
-        "type": "online",
-        "url": "https://yahoo.com"
-    })
-    assert sorted(await get_services()) == sorted(['redis', 'google', 'yahoo'])
-    assert sorted(await get_service_config(service_name='yahoo')) == sorted({ # type: ignore
-        "service_name": "yahoo",
-        "type": "online",
-        "url": "https://yahoo.com"
-    })
+# @pytest.mark.asyncio
+# async def test_load_basic_servies():
+#     """
+#     test load service
+#     """
+#     assert sorted(await get_services()) == sorted(['redis', 'google'])
 
 
-@pytest.mark.asyncio
-async def test_remove_servies():
-    """
-    test remove a service
-    """
-    await remove_service(name='yahoo')
-    assert sorted(await get_services()) == ['google', 'redis']
-    assert await get_service_config('yahoo') == {}
+# @pytest.mark.asyncio
+# async def test_add_servies():
+#     """
+#     test add new service
+#     """
+#     await add_service({
+#         "service_name": "yahoo",
+#         "type": "online",
+#         "url": "https://yahoo.com"
+#     })
+#     assert sorted(await get_services()) == sorted(['redis', 'google', 'yahoo'])
+#     assert sorted(await get_service_config(service_name='yahoo')) == sorted({ # type: ignore
+#         "service_name": "yahoo",
+#         "type": "online",
+#         "url": "https://yahoo.com"
+#     })
 
 
-# s = r.smembers('services')
-# services =  [i.decode(encoding='utf-8') for i in s]
+# @pytest.mark.asyncio
+# async def test_remove_servies():
+#     """
+#     test remove a service
+#     """
+#     await remove_service(name='yahoo')
+#     assert sorted(await get_services()) == ['google', 'redis']
+#     assert await get_service_config('yahoo') == {}
 
-# for i in services:
-#     r.srem("services", i)
-#     r.delete(i)
-#     r.delete(f"{i}_status")
+
+# # s = r.smembers('services')
+# # services =  [i.decode(encoding='utf-8') for i in s]
+
+# # for i in services:
+# #     r.srem("services", i)
+# #     r.delete(i)
+# #     r.delete(f"{i}_status")
